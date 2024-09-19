b334c117d41c4a407198778beee581e94f87744d
Ramnath.T.Chekka <ch.ramnathteja@gmail.com>
2024-09-19 22:22:33 +0530

    adding beans to spring context using @Component

--- src/main/java/com/tutorial/springcontextexample/config/ProjectConfig.java
+++ src/main/java/com/tutorial/springcontextexample/config/ProjectConfig.java
@@ -1,34 +1,34 @@
 package com.tutorial.springcontextexample.config;
 
-import com.tutorial.springcontextexample.Parrot;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
 @Configuration
+@ComponentScan(basePackages = "com.tutorial.springcontextexample.main")
 public class ProjectConfig {
 
-    @Bean
-    @Primary
-    Parrot parrot() {
-        var parrot = new Parrot();
-        parrot.setName("KOKO");
-        return parrot;
-    }
-
-    @Bean
-    Parrot parrot2() {
-        var parrot = new Parrot();
-        parrot.setName("Miki");
-        return parrot;
-    }
-
-    @Bean(name = "riki")
-    Parrot parrot3() {
-        var parrot = new Parrot();
-        parrot.setName("Riki");
-        return parrot;
-    }
+//    @Bean
+//    @Primary
+//    Parrot parrot() {
+//        var parrot = new Parrot();
+//        parrot.setName("KOKO");
+//        return parrot;
+//    }
+//
+//    @Bean
+//    Parrot parrot2() {
+//        var parrot = new Parrot();
+//        parrot.setName("Miki");
+//        return parrot;
+//    }
+//
+//    @Bean(name = "riki")
+//    Parrot parrot3() {
+//        var parrot = new Parrot();
+//        parrot.setName("Riki");
+//        return parrot;
+//    }
 
     @Bean
     String hello() {
--- src/main/java/com/tutorial/springcontextexample/main/Main.java
+++ src/main/java/com/tutorial/springcontextexample/main/Main.java
@@ -1,6 +1,5 @@
 package com.tutorial.springcontextexample.main;
 
-import com.tutorial.springcontextexample.Parrot;
 import com.tutorial.springcontextexample.config.ProjectConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -9,17 +8,23 @@ public class Main {
 
         var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
 
-        Parrot parrot = context.getBean("parrot2",Parrot.class);
+        Parrot parrot = context.getBean(Parrot.class);
+        System.out.println(parrot);
+        System.out.println(parrot.getName());
+        parrot.setName("Kiki");
         System.out.println(parrot.getName());
 
-        Parrot parrotPrimary = context.getBean(Parrot.class);
-        System.out.println(parrotPrimary.getName());
-
-        Parrot parrot2 = context.getBean("riki",Parrot.class);
-        System.out.println(parrot2.getName());
-
-        String string = context.getBean(String.class);
-        System.out.println(string);
+//        Parrot parrot = context.getBean("parrot2",Parrot.class);
+//        System.out.println(parrot.getName());
+//
+//        Parrot parrotPrimary = context.getBean(Parrot.class);
+//        System.out.println(parrotPrimary.getName());
+//
+//        Parrot parrot2 = context.getBean("riki",Parrot.class);
+//        System.out.println(parrot2.getName());
+//
+//        String string = context.getBean(String.class);
+//        System.out.println(string);
 
         Integer integer = context.getBean(Integer.class);
         System.out.println(integer);
